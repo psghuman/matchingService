e4d4d0daa4684014f71b1196ea1e11c026175dc8
Puneet Ghuman <ghumanpuneet97@gmail.com>
2021-03-01 13:36:20 -0500

    find ip code

--- matchingService-go/main.go
+++ matchingService-go/main.go
@@ -4,96 +4,93 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 )
 
-type MatchingResult struct {
-	MatchedIPAddress string `json:"matched_ip_address"`
-	IsServer bool `json:"is_server"`
+//ipRange - a structure that holds the start and end of a range of ip addresses
+type ipRange struct {
+	start net.IP
+	end   net.IP
 }
 
-type MatchingRequest struct {
-	IpAddress string `json:"ip_address"`
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipRange, ipAddress net.IP) bool {
+	// strcmp type byte comparison
+	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+		return true
+	}
+	return false
 }
 
-func sendMatchedInformation(player string, matchedPlayer string, isServer bool) (bool, error) {
-	result := &MatchingResult{
-		MatchedIPAddress: matchedPlayer,
-		IsServer: isServer,
-	}
-	resultBinary, err := json.Marshal(result)
-	if err != nil {
-		return false, err
-	}
-	req, err := http.NewRequest("POST", player, bytes.NewBuffer(resultBinary))
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		return true, err
-	}
-	return false, err
+var privateRanges = []ipRange{
+	ipRange{
+		start: net.ParseIP("10.0.0.0"),
+		end:   net.ParseIP("10.255.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("100.64.0.0"),
+		end:   net.ParseIP("100.127.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("172.16.0.0"),
+		end:   net.ParseIP("172.31.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.0.0.0"),
+		end:   net.ParseIP("192.0.0.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.168.0.0"),
+		end:   net.ParseIP("192.168.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("198.18.0.0"),
+		end:   net.ParseIP("198.19.255.255"),
+	},
 }
 
-func findMatch(playerIPAddresses <-chan string) {
-	for {
-		ipAddr1 := <-playerIPAddresses
-		ipAddr2 := <-playerIPAddresses
-		success, err := sendMatchedInformation(ipAddr1, ipAddr2, true)
-		if !success {
-			log.Println("Could not send request to Player with IP: " + ipAddr1)
-			log.Println(err)
-			// TODO: add player2 back to the pool
-			continue
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ipAddress net.IP) bool {
+	// my use case is only concerned with ipv4 atm
+	if ipCheck := ipAddress.To4(); ipCheck != nil {
+		// iterate over all our ranges
+		for _, r := range privateRanges {
+			// check if this ip is in a private range
+			if inRange(r, ipAddress) {
+				return true
+			}
 		}
-		success, err = sendMatchedInformation(ipAddr2, ipAddr1, false)
-		if !success {
-			// TODO: add player1 back to the pool
-			log.Println("Could not send request to Player with IP: " + ipAddr2)
-			log.Println(err)
-		}
-	}	
+	}
+	return false
 }
 
-func addPlayerHandler(w http.ResponseWriter, r *http.Request, playerIPAddresses chan<- string) {
-	switch r.Method {
-	case "POST":
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			panic(err)
+func getIPAdress(w http.ResponseWriter, r *http.Request) string {
+	for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+		addresses := strings.Split(r.Header.Get(h), ",")
+		// march from right to left until we get a public address
+		// that will be the address right before our proxy.
+		for i := len(addresses) - 1; i >= 0; i-- {
+			ip := strings.TrimSpace(addresses[i])
+			// header can contain spaces too, strip those out.
+			realIP := net.ParseIP(ip)
+			if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
+				// bad address, go to next
+				continue
+			}
+			w.WriteHeader(http.StatusOK)
+			return ip
 		}
-		log.Println(string(body))
-		var m MatchingRequest
-		err = json.Unmarshal(body, &m)
-		if err != nil {
-			panic(err)
-		}
-		log.Println(m)
-		resp := map[string]string{
-			"message": "Successfully added to pool",
-		}
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		_ = json.NewEncoder(w).Encode(resp)
-		playerIPAddresses <- m.IpAddress
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "Only POST method supported for matching request.")
 	}
+	w.WriteHeader(http.StatusOK)
+	return ""
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	resp := map[string]string{
-		"message": "Service is running at 8080 port",
+		"message": "Service is running at 8001 port",
 	}
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -101,18 +98,16 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	playerIPAddresses := make(chan string, 100)
-	go findMatch(playerIPAddresses)
 	// server setup
 	mux := http.NewServeMux()
 	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		healthCheck(w, r)
 	}))
 	mux.Handle("/findMatch", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		addPlayerHandler(w, r, playerIPAddresses)
+		fmt.Println(getIPAdress(w, r))
 	}))
 	srv := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8001",
 		Handler: mux,
 	}
 	log.Println("starting server!")
